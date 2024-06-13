279d6463852518f695692606c44ce4347b1901db
Michael Freeman <mikef@Michaels-MacBook-Pro.local>
2024-06-13 12:12:01 -0400

    added model for user

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/, 'Enter a Valid Email']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtiuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function(){
+        return `${this.friends.length}`
+    });
+
+    const User = model('user', userSchema);
+
+    module.exports = User;
