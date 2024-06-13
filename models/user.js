2c4597f5159708b03382f7b6b67b4183bd685139
Michael Freeman <mikef@Michaels-MacBook-Pro.local>
2024-06-13 12:15:01 -0400

    added comments on user model

--- models/user.js
+++ models/user.js
@@ -1,25 +1,30 @@
+//require mongoose for schema and model
 const { Schema, model } = require('mongoose');
-
+//creating user schema
 const userSchema = new Schema(
     {
+        // make username portion of model
         username: {
             type: String,
             required: true,
             unique: true,
             trim: true,
         },
+        //email portion of model
         email: {
             type: String,
             required: true,
             unique: true,
             match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/, 'Enter a Valid Email']
         },
+        //thoughts portion of model
         thoughts: [
             {
                 type: Schema.Types.ObjectId,
                 ref: 'thought'
             },
         ],
+        //friends portion of model
         friends: [
             {
                 type: Schema.Types.ObjectId,
@@ -28,17 +33,18 @@ const userSchema = new Schema(
         ],
     },
     {
+        //allowing virtuals
         toJSON: {
             virtiuals: true,
         },
         id: false,
     }
 );
-
+// creating virtual for gathering friends length
 userSchema.virtual('friendCount').get(function(){
         return `${this.friends.length}`
     });
-
+// initialize user model
     const User = model('user', userSchema);
-
+// export user model
     module.exports = User;
