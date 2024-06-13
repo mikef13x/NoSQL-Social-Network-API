0f8e44bb49a02957b50c0d86b0eaa7c548c3ed8a
Michael Freeman <mikef@Michaels-MacBook-Pro.local>
2024-06-13 13:20:22 -0400

    added comments to index model file

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
+//require user model
 const User = require('./user')
+//require thought model
 const Thought = require('./thought')
 
-
+// export both user and thought model
 module.exports = { User, Thought };
